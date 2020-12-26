dd513fbb834ddef4ee810467f73d0673a5ba979b
alexwhitmarsh <alexwhitmarsh@yahoo.co.uk>
2020-12-26 22:08:32 +0000

    End July 2020 updates

--- Bioresources_market_information.py
+++ Bioresources_market_information.py
@@ -1,13 +1,17 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from itertools import permutations
+from itertools import combinations
+import Bioresources_market_info_functions as bio
+
+# ------------  INITIATION --------------------------------------------------
+path = r''
 
 company = ['ANH', 'SVT', 'SWB', 'TMS', 'UUW', 'WSX']
+df_WwTW = pd.DataFrame()
+df_STC = pd.DataFrame()
+df4 = pd.DataFrame()
+df_mega = pd.DataFrame()
 
-df=pd.DataFrame()
-
+# -----------  GRAB THE RELEVANT HEADERS TO BE USED IN THE DATAFRAME -------------------
 # Grab the column names from the Anglian sheet (TMS and possibly others had changed there's, causing problems)
 headers_WwTW = pd.read_excel(r'C:\Users\Jacob\OneDrive\Python\Pycharm\Bioresources market information\ANH.xlsx',
                              header=None, sheet_name='WwTW', skiprows=5, usecols=('D:F, H:M, O:R, T:X'),
@@ -16,59 +20,61 @@ headers_WwTW = pd.read_excel(r'C:\Users\Jacob\OneDrive\Python\Pycharm\Bioresourc
 headers_STC = pd.read_excel(r'C:\Users\Jacob\OneDrive\Python\Pycharm\Bioresources market information\ANH.xlsx',
                             header=None, sheet_name='STC', skiprows=5, usecols=('D:F, H:O, Q:S, U:X'))
 
-def f(df, headers):
-    df.columns = headers.iloc[0, :].tolist()
-    df.set_index(pd.MultiIndex.from_product([[company[i]], df.index]), inplace=True)
-    df.dropna(thresh=5, inplace=True)
-    df['Company'] = company[i]
-    return df
-
 # Whack the data together into a big data from using a for loop
 for i in range(len(company)):
-    path = r'C:\Users\Jacob\OneDrive\Python\Pycharm\Bioresources market information\\'
-    path += company[i] + ".xlsx"
+    path = fr'C:\Users\Jacob\OneDrive\Python\Pycharm\Bioresources market information\inputs\\{company[i]}.xlsx'
 
-    df_WwTW = pd.read_excel(path, header=None, sheet_name='WwTW', skiprows=10, usecols=('D:F, H:M, O:R, T:X'), )
-    df_WwTW = f(df_WwTW, headers_WwTW)
+    df_temp = pd.read_excel(path, header=None, sheet_name='WwTW', skiprows=10, usecols=('D:F, H:M, O:R, T:X'), )
+    df_temp = bio.data_mung(df_temp, headers_WwTW, company[i])
+    df_WwTW = pd.concat([df_WwTW, df_temp])
 
-    #df_STC = pd.read_excel(path, header=None, sheet_name='STC', skiprows=10, usecols=('D:F, H:O, Q:S, U:X'))
-    #df_STC = f(df_STC, headers_WwTW)
+    #This creates a pair of lat/long coordinates to enable analysis later
+    df_WwTW['Coordinates'] = [[df_WwTW['WwTW location grid ref latitude'][i],
+                              df_WwTW['WwTW location grid ref longitude'][i]]
+                              for i in range(len(df_WwTW.index))]
+
+    df_temp2 = pd.read_excel(path, header=None, sheet_name='STC', skiprows=10, usecols=('D:F, H:O, Q:S, U:X'))
+    df_temp2 = bio.data_mung(df_temp2, headers_STC, company[i])
+    df_STC = pd.concat([df_temp2, df_STC])
+
+    # This creates a pair of lat/long coordinates to enable analysis later
+    df_STC['Coordinates'] = [[df_STC['STC location (grid ref latitude)'][i],
+                               df_STC['STC location (grid ref longitude)'][i]]
+                              for i in range(len(df_STC.index))]
 
     #df_merge = pd.merge(df_WwTW, df_STC, left_on='WwTW site name', right_on='WwTW site name', how='outer')
-
-    df = pd.concat([df, df_WwTW])
     #df['Company'] = df.apply(lambda row: row.Company_x if pd.isna(row.Company_x) == False else row.Company_y, axis=1)
 
+# Export the concatenated dataframes to CSV
+df_STC.to_csv('Outputs/bioresources_market_information_STC.csv')
+df_WwTW.to_csv('Outputs/bioresources_market_information_WwTW.csv')
 
-#0. This coverts long and lat into coordinate pairs
-df['Coordinates'] = [[df['WwTW location grid ref latitude'][i], df['WwTW location grid ref longitude'][i]] for i in range(len(df.index))]
 
 
-# 1. This is the list of companies
-companies = df.Company.unique()
 
 # 2. This creates a set of possible company combinations
-lst2 = [ [companies[i], companies[j]] for i in range(len(companies)) for j in range(len(companies)) if i!=j]
-print(lst2)
-# 3.
-df3 = pd.DataFrame()
+company_combo = list(combinations(company, 2))
+comp1 = [company_combo[i][0] for i in range(len(company_combo))] # This extracts the first company in the company combo
+comp2 = [company_combo[i][1] for i in range(len(company_combo))] # This extracts the second company in the company combo
 
-for i in range(len(companies)):
-    df3[lst2[i][0]] = df.loc[lst2[i][0], 'Coordinates']
-    print(df3[lst2[i][0]])
+# This loop applies the 'distance pairings' function to the relevant dataframe to each company combo
+for i, j in zip(comp1, comp2):
+    print(i, j)
+    df = bio.distance_pairings(df_STC, i, j, "Sludge Treatment Centre (STC) name").sort_values(by=['Distance'])
+    df_mega = pd.concat([df_mega, df])
 
-
-#print(df.loc['ANH', 'Coordinates'])
-
-df.to_csv('bioresources_market_information.csv')
+df_mega.to_csv('Outputs/bioresources_market_information_STC_Pairings_MEGA.csv')
 
 
-"""for i in df_STC['WwTW site name']:
+
+
+"""
+for i in range(len(lst)):
+    df3[lst[i][0]] = df.loc[lst[i][0], 'Coordinates']
+
+for i in df_STC['WwTW site name']:
     if i in df_WwTW['WwTW site name'].values:
         print(i)
-"""
-
-
 
 # Group by analysis
 poo= df.groupby(['Company']).agg(['max', 'mean', 'min']).transpose()
@@ -77,7 +83,7 @@ poo2 = poo.swaplevel(0).sort_index(axis=0)
 
 
 # Visualisaitons- pick and choose these
-"""
+
 # Distribution of quantity of sludge produced per year
 plt.hist(df.iloc[:,3].loc['ANH'], bins=50, alpha=0.5, density=True, label='ANH')
 plt.hist(df.iloc[:,3].loc['SVT'], bins=50, alpha=0.5, density=True, label='SVT')
